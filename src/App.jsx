7a9e73db8d33d5362b7a7ccdf92c71a60aa9c771
Anshu Gupta <anshurauniyar2000@gmail.com>
2024-06-28 14:38:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex-1">
+      hi
+      <div>hi</div>
+    </div>
+  );
+}
+
+export default App;
