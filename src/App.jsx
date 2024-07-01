4098d0d96beb4e1934c2c4be14e642bb689491b9
Anshu Gupta <anshurauniyar2000@gmail.com>
2024-07-01 18:27:39 +0530

    applayout-added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Children } from "react";
 
-const router = createBrowserRouter(
-  {
-    element:<AppLayout/>,
-    Children:[]
-  }
-)
+const router = createBrowserRouter({
+  element: <AppLayout />,
+  Children: [],
+});
+function App() {
   return (
     <div className="text-2xl">
       hi
       <div>hi</div>
     </div>
   );
-};
-
+}
 export default App;
